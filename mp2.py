5fdbe4c2d131097a16b5263ea8ce406bdceba32a
punitjha <punit2@illinois.edu>
2017-07-25 20:12:59 -0500

    mp2.ipynb
    The quicke r algorithm for MP2 is to be implemented

--- mp2.py
+++ mp2.py
@@ -2,11 +2,12 @@
 """
 The Second-Order Moller-Plesset Perturbation Theory (MP2) Energy code
 """
-
+#%matplotlib notebook
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy import linalg as LA
 import timing
+import print_perfect
 
 #Please check the number of occupied MO in the molecule
 OCC=5;
@@ -28,10 +29,7 @@ for row in range(overlap.shape[0]):
     val=overlap[row,2]
     over_mat[i1-1,i2-1]=over_mat[i2-1,i1-1]=val
 print ("Overlap Integral Matrix \n \n")
-for row in range(over_mat.shape[0]):
-    for col in range(over_mat.shape[1]):
-        print('{:11.8f}'.format(over_mat[row,col]),end=' ')
-    print()
+print_perfect.print_perfect(over_mat)
 
 
 #The kinetic energy matrix
@@ -43,10 +41,7 @@ for row in range(kinetic.shape[0]):
     val=kinetic[row,2]
     kinetic_mat[i1-1,i2-1]=kinetic_mat[i2-1,i1-1]=val
 print ("\n Kinetic Energy Matrix \n \n")
-for row in range(kinetic_mat.shape[0]):
-    for col in range(kinetic_mat.shape[1]):
-        print('{:11.8f}'.format(kinetic_mat[row,col]),end=' ')
-    print()
+print_perfect.print_perfect(kinetic_mat)
 
 
 #The nuclear attraction matrix
@@ -58,10 +53,7 @@ for row in range(nuk_att.shape[0]):
     val=nuk_att[row,2]
     nuk_mat[i1-1,i2-1]=nuk_mat[i2-1,i1-1]=val
 print ("\n  Nuclear Att. Integrals are  \n \n")
-for row in range(nuk_mat.shape[0]):
-    for col in range(nuk_mat.shape[1]):
-        print('{:11.7f}'.format(nuk_mat[row,col]),end=' ')
-    print()
+print_perfect.print_perfect(kinetic_mat)
     
 
 
@@ -70,10 +62,7 @@ for row in range(nuk_mat.shape[0]):
 #The the core Hamiltonian matrix
 H_core=kinetic_mat+nuk_mat
 print ("\n This is the Core Hamiltonian Matrix \n \n")
-for row in range(H_core.shape[0]):
-    for col in range(H_core.shape[1]):
-        print('{:11.7f}'.format(H_core[row,col]),end=' ')
-    print()
+print_perfect.print_perfect(H_core)
 
 
 
@@ -101,7 +90,12 @@ def index2(i, j, k, l):
     else:
         ijkl=int(index1(kl,ij))
     return int(ijkl)
-    
+#The second index function used for the mp2 program
+def index3(i,j):
+    if (i>j):
+        return index1(i,j)
+    else:
+        return index1(j,i)
 
 
 
@@ -124,10 +118,7 @@ timing.endlog()
 eigval, eigvec=LA.eigh(over_mat)
 print("The eigenvalues are:", "\n \n",eigval)
 print("\n\n\nThe eigenvectors are: \n \n ")
-for row in range(eigvec.shape[0]):
-    for col in range(eigvec.shape[1]):
-        print('{:11.7f}'.format(eigvec[row,col]),end=' ')
-    print()
+print_perfect.print_perfect(eigvec)
 eig_t=eigvec.transpose()
 eigval=eigval**(-1/2)
 eigval_m=np.diag(eigval)
@@ -135,10 +126,7 @@ np.set_printoptions(precision=5)
 print("\n\n\nThe eigenvalues in matrix form:\n\n ",eigval_m)
 s_1by2=eigvec.dot(eigval_m).dot(eig_t)
 print("\n\n\nThe symmetric orthogonalization matrix is:\n\n ")
-for row in range(s_1by2.shape[0]):
-    for col in range(s_1by2.shape[1]):
-        print('{:11.7f}'.format(s_1by2[row,col]),end=' ')
-    print()
+print_perfect.print_perfect(s_1by2)
 
 
 
@@ -148,40 +136,27 @@ for row in range(s_1by2.shape[0]):
 
 i_F=s_1by2.transpose().dot(H_core).dot(s_1by2)
 print("The transformed Fock matrix is :\n \n ")
-for row in range(i_F.shape[0]):
-    for col in range(i_F.shape[1]):
-        print('{:11.7f}'.format(i_F[row,col]),end=' ')
-    print()
+print_perfect.print_perfect(i_F)
 eigval1, eigvec1=LA.eigh(i_F)
 AO_eigvec=s_1by2.dot(eigvec1)
 print("\nThe initial MO coeffcient is  :\n \n ")
-for row in range(AO_eigvec.shape[0]):
-    for col in range(AO_eigvec.shape[1]):
-        print('{:11.7f}'.format(AO_eigvec[row,col]),end=' ')
-    print()
+print_perfect.print_perfect(AO_eigvec)
 
 
 #The initial density matrix
 AO_eigvec=np.delete(AO_eigvec,range(OCC,AO_eigvec.shape[1]),1)
 print("\n\nThe new MO matrix with only the occupied orbitals is\n\n ")
-for row in range(AO_eigvec.shape[0]):
-    for col in range(AO_eigvec.shape[1]):
-        print('{:11.7f}'.format(AO_eigvec[row,col]),end=' ')
-    print()
+print_perfect.print_perfect(AO_eigvec)
+
 
 print("\n\nThe new MO matrix (transpose) is\n\n ")
-for row in range(AO_eigvec.transpose().shape[0]):
-    for col in range(AO_eigvec.transpose().shape[1]):
-        print('{:11.7f}'.format(AO_eigvec.transpose()[row,col]),end=' ')
-    print()
+print_perfect.print_perfect(AO_eigvec.transpose())
 
 
 i_Den=AO_eigvec.dot(AO_eigvec.transpose())
 print("\nThe initial density matrix is  :\n \n ")
-for row in range(i_Den.shape[0]):
-    for col in range(i_Den.shape[1]):
-        print('{:11.7f}'.format(i_Den[row,col]),end=' ')
-    print()
+print_perfect.print_perfect(i_Den)
+
     
 #Computing the Initial SCF Energy
 ene1=0.0
@@ -218,6 +193,7 @@ for x in range(1000):
 
             new_fock=s_1by2.transpose().dot(fock).dot(s_1by2)
             eigval2, eigvec2=LA.eigh(new_fock)
+            eps=eigval2
             AO_eigvec2=s_1by2.dot(eigvec2)
             eig_AO_store=AO_eigvec2
             AO_eigvec2=np.delete(AO_eigvec2,range(OCC,AO_eigvec2.shape[1]),1)
@@ -243,44 +219,37 @@ for x in range(1000):
             ene=ene_new
 timing.endlog()
 
-
-
-
+#####################################################
 plt.plot(xx,ene222, '--',linewidth=1)
 plt.xlim(0,25)
 plt.ylim(-76,-85)
-
+#####################################################
 
 
 
 #This implements the extras in Project 3 - The molecular orbital basis to check whether transformation is 
 #actually required or not.
-
+import timing
 
 print("\nThe final coefficient matrix obtained form HF is \n")
+print_perfect.print_perfect(eig_AO_store)
 
-for row in range(eig_AO_store.shape[0]):
-    for col in range(eig_AO_store.shape[1]):
-        print('{:11.7f}'.format(eig_AO_store[row,col]),end=' ')
-    print()
 timing.log('Tracking the matrix multiplication time')
 F_MO2=eig_AO_store.transpose().dot(fock).dot(eig_AO_store)
 timing.endlog()
 print("\nThe Fock matrix in the MO basis is \n")
-for row in range(F_MO2.shape[0]):
-    for col in range(F_MO2.shape[1]):
-        print('{:11.7f}'.format(F_MO2[row,col]),end=' ')
-    print()
+print_perfect.print_perfect(F_MO2)
 
 
-d_M=np.zeros((d_I.size))
+#The noddy algorithm for the transfrom of two electron integrals to the MO basis
+d_M=np.zeros((int((AO*(AO+1)/2)*((AO*(AO+1)/2)+1)/2)))
 
 ijkl=0
 for i in range(AO):
-    for j in range(i):
-        for k in range(i):
+    for j in range(i+1):
+        for k in range(i+1):
             lim=j if (i==k) else k 
-            for l in range(lim):
+            for l in range(lim+1):               
                 for p in range(AO):
                     for q in range(AO):
                         for r in range(AO):
@@ -289,16 +258,88 @@ for i in range(AO):
                                 pqrs=index2(p+1,q+1,r+1,s+1)
                                 #print(ijkl," ",pqrs," ", d_I[pqrs])
                                 d_M[ijkl]+=eig_AO_store[p,i]*eig_AO_store[q,j]*eig_AO_store[r,k]*eig_AO_store[s,l]*d_I[pqrs]
-            ijkl=ijkl+1
-            
+                ijkl=ijkl+1
+
+
+
+X=np.zeros((AO,AO))
+Y=np.zeros((AO,AO))
+TMP=np.zeros((AO*(AO+1)/2,AO*(AO+1)/2))
+d_M_f=np.zeros(((AO*(AO+1)/2)*((AO*(AO+1)/2)+1)/2))
+
+ij=0
+for i in range(AO):
+    for j in range(i+1):
+        kl=0
+        for k in range(AO):
+            for l in range(k+1):
+                ijkl=index3(ij,kl)
+                X[k,l]=X[l,k]=d_I[ijkl]
+                kl=kl+1
+        Y=np.zeros((AO,AO))
+        Y=eig_AO_store.transpose().dot(X)
+        X=np.zeros((AO,AO))
+        X=Y.dot(eig_AO_store)
+        kl=0
+        for k in range(AO):
+            for l in range(k+1):
+                TMP[kl,ij]=X[k,l]
+                kl=kl+1
+    ij=ij+1
+
+
+
+kl=0
+for k in range(AO):
+    for l in range(k+1):
+        Y=np.zeros((AO,AO))
+        X=np.zeros((AO,AO))
+        ij=0
+        for i in range(AO):
+            for j in range (i+1):
+                X[i,j]=X[j,i]=TMP[kl][ij]
+                ij=ij+1
+        Y=np.zeros((AO,AO))
+        Y=eig_AO_store.transpose().dot(X)
+        X=np.zeros((AO,AO))
+        X=Y.dot(eig_AO_store)
+        ij=0
+        for i in range (AO):
+            for j in range(i+1):
+                klij=index3(kl,ij)
+                print("this is x[i,j]",X[i,j]," ",klij)
+                d_M_f[klij]=X[i,j]
+                ij=ij+1
+    kl=kl+1
+        
 np.set_printoptions(threshold=10000)                       
-for a in range(d_M.size):
-    print(a,"  ", d_M[a])
+for a in range(d_M_f.size):
+    print(a,"  ", '{:11.8f}'.format(d_M_f[a]))       
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+emp2=0.0
+for i in range(OCC):
+    for a in range(OCC,AO):
+        for j in range(OCC):
+            for b in range(OCC,AO):
+                iajb=index2(i,a, j, b)
+                ibja=index2(i,b, j, a)
+                emp2+=d_M_f[iajb]*(2*d_M_f[iajb]-d_M_f[ibja])/(eps[i]+eps[j]-eps[a]-eps[b])
+
+print("This is the mp2 energy",emp2)
 
 
 
