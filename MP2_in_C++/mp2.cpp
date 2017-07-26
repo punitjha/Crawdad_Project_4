b4130521dd57ab3bc9dd297762b17754feeb7431
punitjha <punit2@illinois.edu>
2017-07-25 20:15:14 -0500

    Conversion:Mulliken to 4D vector failed

--- MP2_in_C++/mp2.cpp
+++ MP2_in_C++/mp2.cpp
@@ -2,6 +2,7 @@
 
 #include "mmult.h"
 #include "hartree.h"
+#include "4d_eri.h"
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -16,7 +17,8 @@
 #include <math.h>
 #include <armadillo>
 #include <complex>
-
+#include "boost/multi_array.hpp"
+#include <cassert>
 #include <array>   // to be able to use this library compile using this g++ -std=c++0x FileNames
 using namespace std;
 
@@ -66,87 +68,74 @@ int main()
 			}
 		}
 	}
-	for (int i=0; i<d_MO.n_elem; i++)
-	{
-		cout<<i<<"  "<<d_MO(i)<<endl;
-	}
+//*****************************************************************************
+//This is the implementation of the better transformation algorithm
+//*****************************************************************************
+//	arma::mat X=arma::zeros(nao,nao);
+//	arma::mat Y=arma::zeros(nao,nao);
+//	arma::mat TMP=arma::zeros(nao*(nao+1)/2,nao*(nao+1)/2);
+//	arma::vec d_MO_f=arma::zeros((nao*(nao+1)/2)*((nao*(nao+1)/2)+1)/2);
+//	int i,j,k,l,ij,kl,klij;
+//	ijkl=0;
+//        for(i=0,ij=0; i<nao; i++)
+//		for(j=0; j<=i; j++,ij++){
+//			for(k=0,kl=0; k<nao; k++)
+//				for(l=0; l<=k;l++,kl++){
+//					ijkl=index2(ij,kl);
+//					X(k,l)=X(l,k)=d_I(ijkl);
+//				}
+//			Y=arma::zeros(nao,nao);
+//			Y=coeff_car.t()*X;
+//			//mmult(coeff_car,1,X,0,Y,nao,nao,nao);
+//			Y.print();
+//			X=arma::zeros(nao,nao);
+//			//X=Y*coeff_car;
+//			mmult(Y,0,coeff_car,0,X,nao,nao,nao);
+//			for(k=0, kl=0; k<nao; k++)
+//				for(l=0; l<=k; l++, kl++)
+//					TMP(kl,ij)=X(k,l);
+//	}
+//	for(k=0, kl=0; k<nao; k++)
+//		for(l=0; l<= k; l++, kl++){
+//			X=arma::zeros(nao,nao);
+//			Y=arma::zeros(nao,nao);
+//			for(i=0, ij=0; i<nao; i++)
+//				for(j=0; j<=i; j++, ij++)
+//					X(i,j)=X(j,i)=TMP(kl,ij);
+//			Y=arma::zeros(nao,nao);
+//			//Y=coeff_car.t()*X;
+//			mmult(coeff_car,1,X,0,Y,nao,nao,nao);
+//			X=arma::zeros(nao,nao);
+//			//X=Y*coeff_car;
+//			mmult(Y,0,coeff_car,0,X,nao,nao,nao);
+//			for(i=0, ij=0; i<nao; i++)
+//				for(j=0;j<=i;j++,ij++){
+//					klij=index2(kl,ij);
+//					d_MO_f(klij)=X(i,j);
+//				}
+//			}
+//	d_MO_f.print("\n\nThis is the new MO array \n");
+
+
 
 //*****************************************************************************
 //This is the implementation of the better transformation algorithm
 //*****************************************************************************
 
-	arma::mat X=arma::zeros(nao,nao);
-	arma::mat Y=arma::zeros(nao,nao);
-	arma::mat TMP=arma::zeros(nao*(nao+1)/2,nao*(nao+1)/2);
-	arma::vec d_MO_f=arma::zeros((nao*(nao+1)/2)*((nao*(nao+1)/2)+1)/2);
-	//arma::vec d_MO_f=arma::zeros(1000);
-	int i,j,k,l,ij,kl,klij;
-	ijkl=0;
-        for(i=0,ij=0; i<nao; i++)
-	{
-		for(j=0; j<=i; j++,ij++)
-		{
-			for(k=0,kl=0; k<nao; k++)
-			{
-				for(l=0; l<=k;l++,kl++)
-				{
-					ijkl=index2(ij,kl);
-					X(k,l)=X(l,k)=d_I(ijkl);
-				}
-			}
-			Y=arma::zeros(nao,nao);
-			//Y=coeff_car.t()*X;
-			mmult(coeff_car,1,X,0,Y,nao,nao,nao);
-			X=arma::zeros(nao,nao);
-			//X=Y*coeff_car;
-			mmult(Y,0,coeff_car,0,X,nao,nao,nao);
-			for(k=0, kl=0; k<nao; k++)
-			{
-				for(l=0; l<=k; l++, kl++)
-				{
-					TMP(kl,ij)=X(k,l);
-				}
-			}
-		}
-	}
+//	typedef boost::multi_array<double,4> array_type;
+//	typedef array_type::index index;
+//	array_type deri(boost::extents[nao][nao][nao][nao]);
+//	double *p=deri.data();
+//	double (*array)[nao][nao][nao][nao]=(double (*)[nao][nao][nao][nao])p;
 
-	for(k=0, kl=0; k<nao; k++)
-	{
-		for(l=0; l<= k; l++, kl++)
-		{
-			X=arma::zeros(nao,nao);
-			Y=arma::zeros(nao,nao);
-			for(i=0, ij=0; i<nao; i++)
-			{
-				for(j=0; j<=i; j++, ij++)
-				{
-					X(i,j)=X(j,i)=TMP(kl,ij);
-				}
-			}
-			Y=arma::zeros(nao,nao);
-			//Y=coeff_car.t()*X;
-			mmult(coeff_car,1,X,0,Y,nao,nao,nao);
-			X=arma::zeros(nao,nao);
-			//X=Y*coeff_car;
-			mmult(Y,0,coeff_car,0,X,nao,nao,nao);
-			for(i=0, ij=0; i<nao; i++)
-			{
-				for(j=0;j<=i;j++,ij++)
-				{
-					klij=index2(kl,ij);
-					d_MO_f(klij)=X(i,j);
-				}
-			}
-		}
-	}
+	vector<vector<vector<vector<double> > > > deri (nao,vector<vector<vector<double> > >(nao,vector<vector<double> > (nao,vector<double>(nao))));
+	d_eri(deri, nao);
 
-
-	d_MO_f.print("\n\nThis is the new MO array \n");
 //*****************************************************************************
 //This is the implementation of the mp2 algorithm 
 //*****************************************************************************
 	double emp2=0.0;
-	for(i=0; i<OCC; i++)
+	for(int i=0; i<OCC; i++)
 	{
 		for(int j=0;j< OCC; j++)
 		{
